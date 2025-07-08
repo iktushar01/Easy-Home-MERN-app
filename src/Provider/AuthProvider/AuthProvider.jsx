50d66d7d25112c049fd9ce6d16160fccdca103ad
tushar <iktushar2020@gmail.com>
2025-07-08 16:59:53 +0600

    onAuthStateChanged in auth

--- src/Provider/AuthProvider/AuthProvider.jsx
+++ src/Provider/AuthProvider/AuthProvider.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AuthContext } from "../../Context/AuthContext/AuthContext";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../Firebase/firebase.init";
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   const signInUser = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   }; 
 
+  useEffect(()=>{
+    const unSubscribe = onAuthStateChanged(auth, currentUser =>{
+        setUser(currentUser);
+        setLoading(false);
+    });
+    return () =>{
+        unSubscribe();
+    }
+  }, [])
+
   const authInfo = {
+    user,
+    loading,
     createUser,
     signInUser
   };
