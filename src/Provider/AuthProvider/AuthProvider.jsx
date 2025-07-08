dca40c42300d51d930e6d9f4081ef363285c5e07
tushar <iktushar2020@gmail.com>
2025-07-08 16:45:52 +0600

    create user with auth

--- src/Provider/AuthProvider/AuthProvider.jsx
+++ src/Provider/AuthProvider/AuthProvider.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { AuthContext } from '../../Context/AuthContext/AuthContext';
+import React from "react";
+import { AuthContext } from "../../Context/AuthContext/AuthContext";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../Firebase/firebase.init";
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    const authInfo = {
-
-    }
-    return (
-        <AuthContext value={authInfo}>
-            {children}
-        </AuthContext>
-    );
+  const authInfo = {
+    createUser,
+  };
+  return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
 
 export default AuthProvider;
