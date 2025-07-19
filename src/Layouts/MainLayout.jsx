dc810e309e720a2390005640de62b984059866d8
tushar <iktushar2020@gmail.com>
2025-07-19 22:22:32 +0600

    loading status add

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Navbar from "../Shared/Navbar/Navbar";
-import { Outlet } from "react-router";
 import Footer from "../Shared/Footer/Footer";
+import { Outlet, useNavigation } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import LoadingSpinner from "../Componens/Buttons/LoadingSpinner";
 
 const MainLayout = () => {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
     <div>
       <Toaster />
+      {isLoading && <LoadingSpinner />}
       <Navbar />
       <Outlet />
       <Footer />
